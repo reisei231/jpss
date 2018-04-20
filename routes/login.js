c72042cb8b58cb47436f44479ffd022c88bc1964
reisei231 <35661242+reisei231@users.noreply.github.com>
2018-04-20 03:35:44 +0300

    Добавлена регистрация/логин (временный)

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
+var crypto = require('crypto');
+var mongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/";
+const key = "jpss";
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("Cookies: ", req.cookies);
-  res.send('LOGIN PAGE');
+  res.render('login');
 
+});
+router.post('/', function(req, res, next) {
+  hash = crypto.createHmac('sha256', key).update(req.body.password).digest('hex');
+  
+  mongoClient.connect(url, function(err, db) {
+    var collection = db.collection("users");
+    collection.findOne({email: req.body.email, password: hash}, function(err, doc){
+      if(doc){
+        console.log(doc._id);
+        res.cookie('userid', doc._id.toString());
+        res.redirect('../');
+      }
+    })
+  });
+  
 });
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
-
+var crypto = require('crypto');
+var mongoClient = require("mongodb").MongoClient;
+const key = 'jpss';
+var url = "mongodb://localhost:27017/";
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('REGISTER PAGE');
+  res.render('register');
 });
-
+router.post('/', function(req, res,next) {
+  hash = crypto.createHmac('sha256', key).update(req.body.password).digest('hex');
+  
+  mongoClient.connect(url, function(err, db) {
+    var collection = db.collection("users");
+    collection.findOne({email: req.body.email},
+      function(err,doc) {
+        if(err){
+          console.log(err);
+        }
+        /*
+        Если пользователя есть в базе данных - отправляем ему сообщение
+        */
+        if(doc){
+          res.render('register', {msg: "Пользователь с таким email-ом уже существует"});
+        }
+        /*
+        Если пользователя нет - сообщаем о успешной регистрации
+        */
+       else{
+         collection.insertOne({email: req.body.email, password: hash}, function(err, result){
+           if(err){
+             console.log(err);
+           }
+           console.log(result.ops);
+           res.redirect('../');// возвращаем пользователя на /index
+         })
+       }
+      })
+  });
+})
 module.exports = router;
